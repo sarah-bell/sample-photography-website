6e8693d00f899be40cbfaddb4c7fe9ac39234d81
Sarah Bell <sarahbell@outlook.com.au>
2018-05-30 20:56:24 +0930

    Added smooth scroll

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Animate smooth scroll
+$('#view-work').on('click', function() {
+  const images = $('#images').position().top;
+  $('html, body').animate({
+    scrollTop: images
+  }, 750);
+});
